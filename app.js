986fb1e9cb1baf5ad0249f0172015f9d8cdb4239
brightpx <dechsaradecho@gmail.com>
2026-07-26 14:17:38 +0700

    DATABASE_PUBLIC_URL

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_PUBLIC_URL,
   ssl: {
     rejectUnauthorized: false
   }
